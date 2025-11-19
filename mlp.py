620576274b1991f86d70302637f22f882dce1eb4
laurjo <laurel.johannson@mail.mcgill.ca>
2025-11-19 13:12:46 -0500

    added k-fold xvalidation, graphs for task1

--- mlp.py
+++ mlp.py
@@ -1,17 +1,19 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 # Load the arrays
 X_train = np.load('X_train.npy')
 y_train = np.load('y_train.npy')
 #the validation set could be used for cross-validation if we decide to implement it
-X_val = np.load('X_val.npy')
-y_val = np.load('y_val.npy')
+#X_val = np.load('X_val.npy')
+#y_val = np.load('y_val.npy')
 X_test = np.load('X_test.npy')
 y_test = np.load('y_test.npy')
 
 print("Dataset shapes:")
 print(f"X_train: {X_train.shape}, y_train: {y_train.shape}")
-print(f"X_val: {X_val.shape}, y_val: {y_val.shape}")
+#print(f"X_val: {X_val.shape}, y_val: {y_val.shape}")
 print(f"X_test: {X_test.shape}, y_test: {y_test.shape}")
 
 #activation functions and their derivatives
@@ -179,7 +181,7 @@ class MLP:
         
         return grads_w, grads_b
 
-    def fit(self, X_train, y_train, X_val, y_val, learning_rate=0.01, epochs=100, batch_size=64):
+    def fit(self, X_train, y_train, X_test, y_test, learning_rate=0.01, epochs=100, batch_size=64):
         """
         Train the MLP using mini-batch gradient descent
         """
@@ -187,8 +189,8 @@ class MLP:
         n_batches = n_samples // batch_size
 
         train_losses = []
-        val_losses = []
-        val_accuracies = []
+        train_accuracies = []
+        test_accuracies = []
 
         for epoch in range(epochs):
             #shuffle training data
@@ -225,16 +227,16 @@ class MLP:
             epoch_loss /= n_batches
             train_losses.append(epoch_loss)
 
-            #validation
-            val_pred, _ = self.forward(X_val)
-            val_loss = cross_entropy_loss(val_pred, y_val)
-            val_losses.append(val_loss)
+            yh = self.predict(X_train)
+            train_acc = self.evaluate_acc(y_train,yh)
+            train_accuracies.append(train_acc)
 
-            yh = self.predict(X_val)
-            val_acc = self.evaluate_acc(y_val,yh)
-            val_accuracies.append(val_acc)
+            #test/validation
+            yh = self.predict(X_test)
+            test_acc = self.evaluate_acc(y_test,yh)
+            test_accuracies.append(test_acc)
 
-        return train_losses, val_losses, val_accuracies
+        return train_losses, train_accuracies, test_accuracies
 
     def predict(self, X):
         """
@@ -251,11 +253,93 @@ class MLP:
         accuracy = np.mean(predictions == y)
         return accuracy
 
-model = MLP(relu, depth=1, m=50)
+def cross_validation_split(n, n_folds=5):
+    #get the number of data samples in each split
+    n_val = n // n_folds
+    inds = np.random.permutation(n)
+    inds = []
+    for f in range(n_folds):
+        tr_inds = []
+        #get the validation indexes
+        val_inds = list(range(f * n_val, (f+1)*n_val))
+        #get the train indexes
+        if f > 0:
+            tr_inds = list(range(f*n_val))
+        if f < n_folds - 1:
+            tr_inds = tr_inds + list(range((f+1)*n_val, n))
+        #The yield statement suspends function’s execution and sends a value back to the caller
+        #but retains enough state information to enable function to resume where it is left off
+        yield tr_inds, val_inds
 
-train_losses, val_losses, val_accuracies = model.fit(X_train, y_train, X_val, y_val,learning_rate=0.01,epochs=50,batch_size=64)
+def kfold_cross_val(x , y, n_folds , model, lr, n_epoches):
+    score_val = np.zeros(n_folds)
+    for f, (tr, val) in enumerate(cross_validation_split(x.shape[0], n_folds)):
+        train_losses, train_accuracies, test_accuracies = model.fit(x[tr], y[tr], x[val], y[val], learning_rate=lr, epochs=n_epoches)
+        score_val[f] = model.evaluate_acc(y[val], model.predict(x[val]))
+    return score_val, score_val.mean()
 
-#test accuracy
-yh = model.predict(X_test)
-accuracy = model.evaluate_acc(y_test, yh)
-print(f"\nFinal Test Accuracy: {accuracy:.4f} for model with {model.depth} hidden layers and {model.m} hidden units per layer")
+
+#initial tuning for #of epochs:
+# for i in range(3):
+#     model = MLP(relu, depth=i, m=256)
+#     train_losses, train_accuracies, test_accuracies = model.fit(X_train, y_train, X_test, y_test,learning_rate=0.01,epochs=100)
+#     plt.plot(train_losses)
+#     plt.ylabel("Training Loss")
+#     plt.xlabel("Epochs")
+#     plt.title("Hidden Layers = "+ str(i))
+#     fig, ax = plt.subplots(figsize=(5, 4), layout='constrained')
+#     ax.plot(test_accuracies, label="Testing Set")
+#     ax.plot(train_accuracies, label = "Training Set")
+#     ax.set_xlabel("Epochs")
+#     ax.set_ylabel("Accuracy")
+#     ax.set_title("Hidden Layers = "+ str(i))
+#     ax.legend()
+#     plt.show()
+
+#k-fold cross validation for learning rate:
+#learing_rate = np.array([1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1])
+
+# loop over learning rates and number of hidden layers
+# for x in range(3):
+#     mn = MLP(relu, depth=2, m=256)
+#     best_score = 0
+#     n=0
+#     accs=np.zeros(len(learing_rate))
+#     for i in learing_rate:
+#             score = kfold_cross_val(X_train,y_train, 5, mn, i, 60)[1]
+#             accs[n]= score
+#             n+=1
+#             if score > best_score:
+#                 best_score = score
+#                 best_lr = i
+#             print(f'for learning_rate= {i}  => score = {score}, best score = {best_score}')
+#     print(f'Best learning_rate: {best_lr}')
+
+#     plt.plot(learing_rate, accs)
+#     plt.ylabel('Accuracy')
+#     plt.xlabel('Learning Rate')
+#     plt.show()
+
+opt_rate = [0.005, 0.01, 0.01]
+for i in range(3):
+    model = MLP(relu, depth=i, m=256)
+    train_losses, train_accuracies, test_accuracies = model.fit(X_train, y_train, X_test, y_test,learning_rate=opt_rate[i],epochs=100)
+    plt.plot(train_losses)
+    plt.ylabel("Training Loss")
+    plt.xlabel("Epochs")
+    plt.title("Hidden Layers = "+ str(i))
+    fig, ax = plt.subplots(figsize=(5, 4), layout='constrained')
+    ax.plot(test_accuracies, label="Testing Set")
+    ax.plot(train_accuracies, label = "Training Set")
+    ax.set_xlabel("Epochs")
+    ax.set_ylabel("Accuracy")
+    ax.set_title("Hidden Layers = "+ str(i))
+    ax.legend()
+    plt.show()
+    yh_train = model.predict(X_train)
+    yh_test = model.predict(X_test)
+    test_acc = model.evaluate_acc(y_test, yh_test)
+    train_acc = model.evaluate_acc(y_train, yh_train)
+    print(f"{i} Hidden Layers:")
+    print(f"Final test acc = {test_acc}")
+    print(f"Final test acc = {train_acc}")
